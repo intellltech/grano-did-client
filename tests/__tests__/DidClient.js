3d678adc41b49c32dc7184c9494314a52b2ce565
intellltech <danieljames.maxdev@gmail.com>
2022-09-04 19:19:08 +0900

    add test for upload

--- tests/__tests__/DidClient.js
+++ tests/__tests__/DidClient.js
@@ -1,6 +1,8 @@
 // @ts-check
 'use strict'
 
+const objectContaining = expect.objectContaining
+
 const DidClient = require('../../lib/DidClient')
 const { mockDidConfig } = require('../mocks/MockDidConfig')
 
@@ -38,6 +40,41 @@ describe('DidClient', () => {
       const chainId = await client.getChainId()
 
       expect(chainId).toBe('did-1')
+    })
+  })
+})
+
+describe('DidClient', () => {
+  describe('.upload()', () => {
+    describe('upload successfully', () => {
+      const tables = [
+        {
+          wasmPath: './wasm/did_contract.wasm',
+          expected: objectContaining({
+            originalSize: expect.any(Number),
+            originalChecksum: expect.any(String),
+            compressedSize: expect.any(Number),
+            compressedChecksum: expect.any(String),
+            codeId: expect.any(Number),
+            height: expect.any(Number),
+            logs: expect.any(Array),
+            transactionHash: expect.any(String),
+            gasWanted: expect.any(Number),
+            gasUsed: expect.any(Number)
+          })
+        }
+      ]
+
+      test.each(tables)('wasmPath: $wasmPath', async ({
+        wasmPath,
+        expected,
+      }) => {
+        const client = await DidClient.createFulfilled(
+          mockDidConfig
+        )
+        const response = await client.upload(wasmPath)
+        expect(response).toEqual(expected)
+      })
     })
   })
 })
