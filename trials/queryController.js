e4e5f2f1a157a740abaac25ff76bfc1bb1760ec5
intellltech <danieljames.maxdev@gmail.com>
2022-11-06 16:18:41 +0900

    update trial

---
+++ trials/queryController.js
@@ -0,0 +1,30 @@
+// @ts-check
+'use strict'
+
+const GranoDidClient = require('../lib/GranoDidClient')
+const GranoDidConfig = require('../lib/GranoDidConfig')
+const { mockGranoDidConfig } =require('./../tests/mocks/MockGranoDidConfig')
+
+
+const main = async () => {
+  const granoDidClient = await GranoDidClient.createFulfilled({
+    config: mockGranoDidConfig,
+  })
+  const wasmPath = './wasm/did_contract.wasm'
+  const result = await granoDidClient.upload(wasmPath)
+
+  const instantiateParams = {
+    codeId: result.codeId
+  }
+  const instantiateResult = await granoDidClient.instantiate(instantiateParams)
+
+  const controllerParams = {
+    contractAddress: instantiateResult.contractAddress,
+    identifier: 'wasm1edz508tre9d9n9nf8r82prpafhds9v6dp0su0y',
+  }
+  const identityOwnerQueryResult = await granoDidClient.controller(controllerParams)
+  console.log(identityOwnerQueryResult)
+}
+
+main()
+
