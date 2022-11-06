24b4cf8c079f8ca4c6fceb80527436637f8116b9
intellltech <danieljames.maxdev@gmail.com>
2022-11-06 17:35:40 +0900

    add executeSetAttribute

---
+++ trials/executeSetAttribute.js
@@ -0,0 +1,34 @@
+// @ts-check
+'use strict'
+
+const GranoDidClient = require('../lib/GranoDidClient')
+const GranoDidConfig = require('../lib/GranoDidConfig')
+const { mockGranoDidConfig } =require('./../tests/mocks/MockGranoDidConfig')
+
+
+const main = async () => {
+  const granoDidClient = await GranoDidClient.createFulfilled({
+    config: mockGranoDidConfig,
+  })
+  const wasmPath = './wasm/did_contract.wasm'
+  const result = await granoDidClient.upload(wasmPath)
+
+  const instantiateParams = {
+    codeId: result.codeId
+  }
+  const instantiateResult = await granoDidClient.instantiate(instantiateParams)
+
+  const setAttributeParams = {
+    contractAddress: instantiateResult.contractAddress,
+    identifier: 'wasm14fsulwpdj9wmjchsjzuze0k37qvw7n7a7l207u',
+    name: 'age',
+    value: '20',
+    validity: 3600 * 24 // second
+  }
+
+  const setAttributeResult = await granoDidClient.setAttribute(setAttributeParams)
+  console.dir(setAttributeResult, { depth: null })
+}
+
+main()
+
