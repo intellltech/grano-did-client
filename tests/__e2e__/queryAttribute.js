137d6aac0588db904c76095cbd8f016910480924
intellltech <danieljames.maxdev@gmail.com>
2022-11-12 16:36:56 +0900

    add test for queryAttribute

--- tests/__e2e__/queryAttribute.js
+++ tests/__e2e__/queryAttribute.js
@@ -7,21 +7,28 @@ const GranoDidClient = require('../../lib/GranoDidClient')
 const { mockGranoDidConfig } = require('../mocks/MockGranoDidConfig')
 
 describe('GranoDidClient', () => {
-  describe('#controller', () => {
+  describe('#attribute', () => {
     describe('success', () => {
       const tables = [
         {
           params: {
             codeId: 1,
             identifier: 'wasm1y0k76dnteklegupzjj0yur6pj0wu9e0z35jafv',
+            name: 'service',
           },
-          expected: {
-            controller: 'wasm1y0k76dnteklegupzjj0yur6pj0wu9e0z35jafv',
-          }
+          expected: { values: [] },
+        },
+        {
+          params: {
+            codeId: 1,
+            identifier: 'wasm1y0k76dnteklegupzjj0yur6pj0wu9e0z35jafv',
+            name: 'service.id',
+          },
+          expected: { values: [] },
         }
       ]
 
-      test.each(tables)('params: $params', async ({
+      test.each(tables)('params:$params', async ({
         params,
         expected,
       }) => {
@@ -35,12 +42,16 @@ describe('GranoDidClient', () => {
         }
         const result = await client.instantiate(instantiateParams)
 
-        const controllerParams = {
-          contractAddress: result.contractAddress,
+        const contractAddress = result.contractAddress
+
+        const attributeParams = {
+          contractAddress: contractAddress,
           identifier: params.identifier,
+          name: params.identifier,
         }
-        const response = await client.controller(controllerParams)
-        expect(response).toEqual(expected)
+        const queryAttributeResult = await client.attribute(attributeParams)
+
+        expect(queryAttributeResult).toEqual(expected)
       })
     })
   })
--- tests/__e2e__/setAttribute.js
+++ tests/__e2e__/setAttribute.js
@@ -13,50 +13,65 @@ describe('GranoDidClient', () => {
         {
           params: {
             codeId: 1,
-            identifier: 'wasm14fsulwpdj9wmjchsjzuze0k37qvw7n7a7l207u',
-            name: 'service.id',
-            value: '#github',
-            validity: 3600 * 24, // second
+            firstRequest: {
+              identifier: 'wasm14fsulwpdj9wmjchsjzuze0k37qvw7n7a7l207u',
+              name: 'service.id',
+              value: '#github',
+              validity: 3600 * 24, // second
+            },
+            secondRequest: {
+              identifier: 'wasm14fsulwpdj9wmjchsjzuze0k37qvw7n7a7l207u',
+              name: 'service.id',
+              value: '#twitter',
+              validity: 3600 * 24, // second
+            },
           },
-          expectedResponse: {
-            logs: expect.any(Array),
-            height: expect.any(Number),
-            transactionHash: expect.any(String),
-            gasWanted: expect.any(Number),
-            gasUsed: expect.any(Number)
-          },
-          expectedWasmEvent: {
-            type: 'wasm',
-            attributes: expect.arrayContaining([
-              {
-                key: 'identifier',
-                value: 'wasm14fsulwpdj9wmjchsjzuze0k37qvw7n7a7l207u',
-              },
-              {
-                key: 'name',
-                value: 'service.id',
-              },
-              {
-                key: 'value',
-                value: '#github',
-              },
-              {
-                key: 'validTo',
-                value: expect.any(String),
-              },
-              {
-                key: 'previousChange',
-                value: '0',
-              },
-            ])
+          expected: {
+            response: {
+              logs: expect.any(Array),
+              height: expect.any(Number),
+              transactionHash: expect.any(String),
+              gasWanted: expect.any(Number),
+              gasUsed: expect.any(Number)
+            },
+            wasmEvent: {
+              type: 'wasm',
+              attributes: expect.arrayContaining([
+                {
+                  key: 'identifier',
+                  value: 'wasm14fsulwpdj9wmjchsjzuze0k37qvw7n7a7l207u',
+                },
+                {
+                  key: 'name',
+                  value: 'service.id',
+                },
+                {
+                  key: 'value',
+                  value: '#github',
+                },
+                {
+                  key: 'validTo',
+                  value: expect.any(String),
+                },
+                {
+                  key: 'previousChange',
+                  value: '0',
+                },
+              ])
+            },
+            firstQueryAttributeResult: {
+              values: ['#github']
+            },
+            secondQueryAttributeResult: {
+              values: ['#github', '#twitter']
+            },
           }
         }
       ]
 
       test.each(tables)('params: $params', async ({
         params,
-        expectedResponse,
-        expectedWasmEvent,
+        expected,
       }) => {
         const client = await GranoDidClient.createFulfilled({
           OriginalSigningCosmWasmClient: SigningCosmWasmClient,
@@ -67,19 +82,43 @@ describe('GranoDidClient', () => {
         }
 
         const result = await client.instantiate(instantiateParams)
+        const contractAddress = result.contractAddress
 
         const setAttributeParams = {
-          contractAddress: result.contractAddress,
-          identifier: params.identifier,
-          name: params.name,
-          value: params.value,
-          validity: params.validity,
+          contractAddress: contractAddress,
+          identifier: params.firstRequest.identifier,
+          name: params.firstRequest.name,
+          value: params.firstRequest.value,
+          validity: params.firstRequest.validity,
         }
 
         const response = await client.setAttribute(setAttributeParams)
-        expect(response).toMatchObject(expectedResponse)
+        expect(response).toMatchObject(expected.response)
         const wasmEvent = response.logs[0].events.find((e) => e.type === 'wasm')
-        expect(wasmEvent).toEqual(expectedWasmEvent)
+        expect(wasmEvent).toEqual(expected.wasmEvent)
+
+        // check queryAttribute
+        const attributeParams = {
+          contractAddress: contractAddress,
+          identifier: params.firstRequest.identifier,
+          name: params.firstRequest.name,
+        }
+        const queryAttributeResult = await client.attribute(attributeParams)
+        expect(queryAttributeResult).toEqual(expected.firstQueryAttributeResult)
+
+        const secondSetAttributeParams = {
+          contractAddress: contractAddress,
+          identifier: params.secondRequest.identifier,
+          name: params.secondRequest.name,
+          value: params.secondRequest.value,
+          validity: params.secondRequest.validity,
+        }
+
+        await client.setAttribute(secondSetAttributeParams)
+
+        // check queryAttribute again
+        const secondQueryAttributeResult = await client.attribute(attributeParams)
+        expect(secondQueryAttributeResult).toEqual(expected.secondQueryAttributeResult)
       })
     })
   })
